8e6e596fe7a20e4846f6019893d47679d3b4f228
schraderSimon <simonsch@uio.no>
2025-05-09 16:38:22 +0200

    slight improvements in data understanding

--- Henon_heiles_GCN/read_data.py
+++ Henon_heiles_GCN/read_data.py
@@ -6,15 +6,20 @@ infile_3D=np.load("nonlinear_coefficients_dimension=3_ngauss_init=28.npz")
 
 times_2D=infile_2D['times']
 times_3D=infile_3D['times']
+
+print(times_2D[0],times_2D[-1],len(times_2D))
+print(times_3D[0],times_3D[-1],len(times_3D))
+num_params_2D=(infile_2D["L"].shape[2]+infile_2D["K"].shape[2]+infile_2D["mu"].shape[2]+infile_2D["p"].shape[2])*infile_2D["L"].shape[1]
+num_params_3D=(infile_3D["L"].shape[2]+infile_3D["K"].shape[2]+infile_3D["mu"].shape[2]+infile_3D["p"].shape[2])*infile_3D["L"].shape[1]
+print("Number of parameters in 2D: ",num_params_2D)
+print("Number of parameters in 3D: ",num_params_3D)
 L_data_2D=infile_2D['L']
 L_data_3D=infile_3D['L']
 K_data_2D=infile_2D['K']
 K_data_3D=infile_3D['K']
-mu_data_2D=infile_2D['mu']
-mu_data_3D=infile_3D['mu']
+q_data_2D=infile_2D['mu']
+q_data_3D=infile_3D['mu']
 p_data_2D=infile_2D['p']
 p_data_3D=infile_3D['p']
-print(L_data_2D.shape)
-print(mu_data_2D.shape)
 plt.plot(times_3D, L_data_3D[:,0,:], label='L (first Gaussian)')
 plt.show()
